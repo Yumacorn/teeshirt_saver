c7cb87e2567a221612b957516228ec598651e078
Ryan Mui <reachryanmui@gmail.com>
2019-03-15 03:41:48 +0000

    Improving menu recursion

--- lib/teeshirt_saver/cli.rb
+++ lib/teeshirt_saver/cli.rb
@@ -10,24 +10,24 @@ class TeeshirtSaver::CLI
     def list_products
       puts "Final Clearance Products"
       @products = TeeshirtSaver::Product.today
+      @products.each.with_index(1) do {product, i}
+        Puts "#{i}. #{product. name} - "product_original price"
     end
 
     def menu
       input = nil
       while input != "exit"
-        input = gets.strip.downcase
         puts "Enter the number of the product you would like to open or type list to see the product list again or type exit to leave:"
-        case input
-          when "1"
-            puts "More info on product 1..."
-          when "2"
-            puts "More info on product 2..."
-          when "list"
-            list_products
-          else
-            puts "Unable to process request, please type list or exit."
-          end
+        input = gets.strip.downcase
+
+        if input.to_i > 0
+          puts @products[input.to_i-1]
+        elsif input == "list"
+          list_products
+        else
+          puts "Unable to process request, please type list or exit."
         end
+      end
     end
 
     def goodbye
