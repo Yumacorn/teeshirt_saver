0855adf7df4f72f87f7789e211ec10208cf47ed2
Ryan Mui <reachryanmui@gmail.com>
2019-03-17 23:50:29 +0000

    Success! Scraped 1 instance make_products

--- lib/teeshirt_saver/scraper.rb
+++ lib/teeshirt_saver/scraper.rb
@@ -11,18 +11,17 @@ class TeeshirtSaver::Scraper
     #Instantiate all products from current WEB
     #Attributes to populate :name, :price, :original_price, :url, :tag
     self.get_products.each do |item|
-      binding.pry
-      #item.children[1].attributes["href"].value = Secret to getting href (almost)
 
-      product = Product.new
+      product = TeeshirtSaver::Product.new
       product.name = item.css("h5").text
-      product.price = item.css(".price sale").text
+      product.price = item.css(".price").text
       product.original_price = item.css(".compare-at-price").text
-      product.url = item.children[1].attributes["href"].value
-      product.tag = item.css(".sale-tag").text
 
-      # product.schedule = item.css(".date").text
-      # product.description = item.css("p").text
+      #Pick URL of product item.children[1].attributes["href"].value = Secret to getting href
+      product.url = "https://www.teeturtle.com" + item.children[1].attributes["href"].value
+      product.tag = item.css(".sale-tag").text
+      binding.pry
+
     end
   end
 
