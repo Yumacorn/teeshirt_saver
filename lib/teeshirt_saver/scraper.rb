f0921941792f7159e278de5e848d9264c1cd7f3b
Ryan Mui <reachryanmui@gmail.com>
2019-03-17 23:44:49 +0000

    In Progress: Found identified for URL of product while scraping

--- lib/teeshirt_saver/scraper.rb
+++ lib/teeshirt_saver/scraper.rb
@@ -12,13 +12,13 @@ class TeeshirtSaver::Scraper
     #Attributes to populate :name, :price, :original_price, :url, :tag
     self.get_products.each do |item|
       binding.pry
-      #item.children[1].attributes = Secret to getting href (almost)
+      #item.children[1].attributes["href"].value = Secret to getting href (almost)
 
       product = Product.new
       product.name = item.css("h5").text
       product.price = item.css(".price sale").text
       product.original_price = item.css(".compare-at-price").text
-      # product.url = item.css("h2").text
+      product.url = item.children[1].attributes["href"].value
       product.tag = item.css(".sale-tag").text
 
       # product.schedule = item.css(".date").text
