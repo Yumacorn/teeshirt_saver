79c542b319b7f4c085ef65c61cb8159bbf0df831
Ryan Mui <reachryanmui@gmail.com>
2019-03-18 00:01:10 +0000

    Removing extraneous code

--- lib/teeshirt_saver/product.rb
+++ lib/teeshirt_saver/product.rb
@@ -2,45 +2,16 @@ class TeeshirtSaver::Product
   attr_accessor :name, :price, :original_price, :url, :tag
   @@all = []
 
-  def initialize
-    @@all << self
-  end
-
   def self.today
-    #List out a set of product instances for today
-    # puts <<-HERE.gsub /^\s*/, ''
-    #   1. LOVE               $9.99       Original: $24      58% OFF
-    #   2. HAY GURL           $9.99       Original: $24      58% OFF
-    # HERE
-
-    # product_1 = self.new
-    # product_1.name = "LOVE"
-    # product_1.price = "$9.99"
-    # product_1.original_price = "$24"
-    # product_1.url = "https://www.teeturtle.com/products/love"
-    # product_1.tag = "58% OFF"
-
-    # product_2 = self.new
-    # product_2.name = "HAY GURL"
-    # product_2.price = "$9.99"
-    # product_2.original_price = "$24"
-    # product_2.url = "https://www.teeturtle.com/products/hay-gurl"
-    # product_2.tag = "58% OFF"
     @@all = TeeshirtSaver::Scraper.make_products
-
-    # @@all = [product_1, product_2]
   end
-
-  # def self.all
-  #   @@all
-  # end
 
   def self.list_products
     #Returns array of product instances to operate on
-    @products = TeeshirtSaver::Product.today
+    products = TeeshirtSaver::Product.today
 
     #Prints list of products array
-    @products.each_with_index {|product, i| display(i+1)}
+    products.each_with_index {|product, i| display(i+1)}
   end
 
   def self.valid_product?(product_number)
